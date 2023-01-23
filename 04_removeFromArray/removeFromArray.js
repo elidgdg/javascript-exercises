05dab5a3aa4f95523c2c82555c9960ee714a35cb
Elijah Gatward <107950467+elidgdg@users.noreply.github.com>
2023-01-23 09:41:31 +0000

    complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
 
+    for (let i = 1; i < arguments.length; i++) {
+        if (array.indexOf(arguments[i]) === -1) continue;
+
+
+        array.splice(array.indexOf(arguments[i]),1);
+    }  
+
+    return array;
 };
 
 // Do not edit below this line
