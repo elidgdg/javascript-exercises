593b9eb2522f2357adcab3562665c8206d6739ca
Elijah Gatward <107950467+elidgdg@users.noreply.github.com>
2023-03-20 20:10:00 +0000

    complete 09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (inputString) {
+    // My worse way
+    // punctuationRegex = /[.,\/#!$%\^&\*;:{}=\-_`~()]/g;
+    // plainText = inputString.replace(punctuationRegex, "").replace(/\s/g, '').toLowerCase();
+    // reversedText = plainText.split("").reverse().join("");
+    // return reversedText === plainText;
 
+
+    // Better solution
+    const processedString = inputString.toLowerCase().replace(/[^a-z]/g, "");
+    return processedString.split("").reverse().join("") == processedString;
 };
 
 // Do not edit below this line
