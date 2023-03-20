87713d64493ba356be5108e857e3d9b5bf0e3835
Elijah Gatward <107950467+elidgdg@users.noreply.github.com>
2023-03-20 20:21:18 +0000

    complete 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(memberNo) {
+    if (memberNo < 0) return "OOPS";
+    if (memberNo === 0) return 0;
+    let a = 1;
+    let b = 1;
+    let c = 0;
+    for (let count = 2; count < memberNo; count++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
 };
 
 // Do not edit below this line
