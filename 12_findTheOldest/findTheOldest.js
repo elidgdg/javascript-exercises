823122f1723ba45bc77b4b6a05f802354d7494b3
Elijah Gatward <107950467+elidgdg@users.noreply.github.com>
2023-03-22 15:26:52 +0000

    complete 12_findtheoldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldestPerson = people.reduce((oldest, current) => {
+        if (oldest.hasOwnProperty('yearOfDeath')) {
+            oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        } else {
+            oldestAge = new Date().getFullYear() - oldest.yearOfBirth;
+        }
+
+        if (current.hasOwnProperty('yearOfDeath')) {
+            currentAge = current.yearOfDeath - current.yearOfBirth;
+        } else {
+            currentAge = new Date().getFullYear() - current.yearOfBirth;
+        }
+
+        if (currentAge > oldestAge) {return current;}
+        return oldest;
+    })
+    return oldestPerson;
 
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
